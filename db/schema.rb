a9e3ae1ea35ce00b76131b6229793b5ff7e563f6
andrewjpage <andrewjpage@gmail.com>
2011-03-11 13:39:06 +0000

    data migration run

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110308142653) do
+ActiveRecord::Schema.define(:version => 20110311120908) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0

d68192e38edfe3a7840d631a876f00b791263647
James Glover <james.glover@sanger.ac.uk>
2015-04-14 12:35:54 +0100

    Correct destruction logic

--- app/models/verification/validator/outdated_plates_scanned.rb
+++ app/models/verification/validator/outdated_plates_scanned.rb
@@ -4,20 +4,20 @@ class Verification::Validator::OutdatedPlatesScanned < ActiveModel::Validator
     plates.keys.each do |barcode|
       plate = plates[barcode]
       if (plate.nil?)
-        record.errors.add(:error, "The barcode #{barcode} hasn't been found")
+        record.errors.add(:error, "The plate #{barcode} hasn't been found")
         next
       end
-      if (plate.plate_purpose.lifespan == 0)
-        record.errors.add(:error, "The barcode #{barcode} can't be destroyed because its a #{plate.plate_purpose.name}")
+      if (plate.plate_purpose.lifespan.nil?)
+        record.errors.add(:error, "The plate #{barcode} can't be destroyed because its a #{plate.plate_purpose.name}")
         next
       end
-      if (!plate.plate_purpose.lifespan.nil?) && (plate.plate_purpose.lifespan < 180)
-        record.errors.add(:error, "The barcode #{barcode} has less than 3 months of lifespan")
+      if (plate.created_at < plate.plate_purpose.lifespan.days.ago )
+        record.errors.add(:error, "The plate #{barcode} is less than #{plate.plate_purpose.lifespan} days old")
         next
       end
     end
     if (plates.empty?)
-      record.errors.add(:base, "None of the barcodes check the conditions to be destroyed")
+      record.errors.add(:base, "No plates found")
     end
   end
 end

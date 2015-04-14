5d36efc0cdb58da3142c0e4f80b47f667e123bf9
James Glover <james.glover@sanger.ac.uk>
2015-04-14 12:39:31 +0100

    Use nice labware destruction name

--- db/migrate/20150202171723_add_destroy_labware.rb
+++ db/migrate/20150202171723_add_destroy_labware.rb
@@ -2,7 +2,7 @@ class AddDestroyLabware < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
       instrument = Instrument.create ({:name => "Destroying instrument", :barcode => "destroying-instrument" })
-      instrument_process = InstrumentProcess.create ({:name => "Destroying labware process", :key => "destroy_labware",
+      instrument_process = InstrumentProcess.create ({:name => "Destroying labware", :key => "destroy_labware",
         :request_instrument => false })
       instrument_processes_instrument = InstrumentProcessesInstrument.create ({:instrument => instrument,
         :instrument_process => instrument_process, :bed_verification_type => "Verification::OutdatedLabware::Base"})

69e3ea7dcd550462519a9db472b8f6b96de9e9ac
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2015-10-28 15:41:18 +0000

    Update by key, instead of name, that is not compatible between environments (uat,nr,prod,...)

--- db/migrate/20151014161021_sets_visual_checks_for_pico.rb
+++ db/migrate/20151014161021_sets_visual_checks_for_pico.rb
@@ -1,12 +1,12 @@
 class SetsVisualChecksForPico < ActiveRecord::Migration
   def self.up
     ActiveRecord::Base.transaction do
-      [*InstrumentProcess.find_by_name("Pico dilution")].each do |ip|
+      [*InstrumentProcess.find_by_key("slf_pico_dilution")].each do |ip|
         ip.update_attributes({
         :visual_check_required => true
       })
       end
-      [*InstrumentProcess.find_by_name("Pico assay plate creation")].each do |ip|
+      [*InstrumentProcess.find_by_key("slf_pico_assay_plates")].each do |ip|
         ip.update_attributes({
         :visual_check_required => true
       })

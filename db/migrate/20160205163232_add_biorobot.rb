202799f626a4a04be829ae408ff66ff98559dd82
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-02-08 10:59:50 +0000

    Adds biorobot instrument and beds configuration

---
+++ db/migrate/20160205163232_add_biorobot.rb
@@ -0,0 +1,37 @@
+class AddBiorobot < ActiveRecord::Migration
+
+  def self.ROBOT_BARCODE
+    "4880001006869"
+  end
+
+  def self.BEDS
+    ['4880001007873', '580000015865',
+     '4880001008658',
+     '4880001009662',
+     '4880001010828']
+  end
+
+  def self.up
+    ActiveRecord::Base.transaction do
+      instrument = Instrument.create! ({:name => "Biorobot", :barcode => self.ROBOT_BARCODE })
+      instrument_process = InstrumentProcess.create! ({:name => "Biorobot", :key => "biorobot",
+        :request_instrument => true })
+      instrument_processes_instrument = InstrumentProcessesInstrument.create! ({:instrument => instrument,
+        :instrument_process => instrument_process, :bed_verification_type => "Verification::DilutionPlate::Biorobot"})
+        self.BEDS.each_with_index.map do |b, pos|
+           Bed.create!({:name => "P#{pos+1}",
+                       :bed_number => pos+1,
+                       :barcode => b,
+                       :instrument => instrument})
+        end
+    end
+  end
+
+  def self.down
+    ActiveRecord::Base.transaction do
+      self.BEDS.each{|b| Bed.find_by_barcode(b).destroy }
+      InstrumentProcess.find_by_name("Biorobot").destroy
+      Instrument.find_by_name("Biorobot").destroy
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20151126153612) do
+ActiveRecord::Schema.define(:version => 20160205163232) do
 
   create_table "beds", :force => true do |t|
     t.string   "name"

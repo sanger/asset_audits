7ee038663e78d72979f917de4fdd5135cdf73c2f
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-02-08 11:02:55 +0000

    Biorobot class to refer to the beds previously created

--- app/models/verification/dilution_plate/biorobot.rb
+++ app/models/verification/dilution_plate/biorobot.rb
@@ -1,6 +1,6 @@
 class Verification::DilutionPlate::Biorobot < Verification::DilutionPlate::Fx
-  self.source_beds      = ['P2']
-  self.destination_beds = ['P3']
+  self.source_beds      = ['P1']
+  self.destination_beds = ['P2']
 
   def self.javascript_partial_name
     "fx_dilution_plate_javascript"

b3cd8f923b353a37af3d6c76bd4210e337e25600
Eduardo Martin Rojo <emr@mib105656i.internal.sanger.ac.uk>
2016-05-23 16:33:54 +0100

    Added new beds and verification for new biorobot process

---
+++ db/migrate/20160523092229_add_qiagen_biorobot.rb
@@ -0,0 +1,41 @@
+class AddQiagenBiorobot < ActiveRecord::Migration
+  ROBOT_BARCODE = "4880001006869"
+  BEDS_CONFIG = {
+     "P1" =>'4880001007873',
+     "P2" =>'580000015865',
+     "P3" =>'4880001008658',
+     "P4" =>'4880001009662',
+     "P5" =>'4880001010828'
+   }
+
+  NEW_BEDS = {
+    "P6" => "580000016879",
+    "P7" => "580000017654"
+  }
+
+  def self.up
+    ActiveRecord::Base.transaction do
+      instrument = Instrument.find_by_barcode!(ROBOT_BARCODE)
+      instrument_process = InstrumentProcess.create! ({:name => "Qiagen / Biorobot", :key => "qiagen_biorobot",
+        :request_instrument => true })
+      instrument_processes_instrument = InstrumentProcessesInstrument.create! ({:instrument => instrument,
+        :instrument_process => instrument_process, :bed_verification_type => "Verification::DilutionPlate::QiagenBiorobot"})
+      Bed.create!({:name => "P6",
+                   :bed_number => 6,
+                   :barcode => NEW_BEDS["P6"],
+                   :instrument => instrument})
+      Bed.create!({:name => "P7",
+                   :bed_number => 7,
+                   :barcode => NEW_BEDS["P7"],
+                   :instrument => instrument})
+    end
+  end
+
+  def self.down
+    ActiveRecord::Base.transaction do
+      Bed.find_by_barcode(NEW_BEDS["P6"]).destroy
+      Bed.find_by_barcode(NEW_BEDS["P7"]).destroy
+      InstrumentProcess.find_by_name("Qiagen / Biorobot").destroy
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20160205163232) do
+ActiveRecord::Schema.define(:version => 20160523092229) do
 
   create_table "beds", :force => true do |t|
     t.string   "name"

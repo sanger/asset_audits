cab45f2439ea29f0bd6ad975ee3bf80deb616379
James Glover <james.glover@sanger.ac.uk>
2016-09-12 10:25:48 +0100

    Remove data migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160617123733) do
+ActiveRecord::Schema.define(version: 20160617122652) do
 
   create_table "beds", force: :cascade do |t|
     t.string   "name",          limit: 255

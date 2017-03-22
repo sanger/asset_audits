14a228e597608a342a762849f05d184368758e65
James Glover <james.glover@sanger.ac.uk>
2017-03-22 13:16:40 +0000

    Simplify has content assertions

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -115,11 +115,7 @@ When /^(?:|I )wait for all AJAX calls$/ do
 end
 
 Then /^(?:|I )should see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_content(text)
-  else
-    assert page.has_content?(text)
-  end
+  assert page.has_content?(text)
 end
 
 Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
@@ -133,11 +129,7 @@ Then /^(?:|I )should see \/([^\/]*)\/$/ do |regexp|
 end
 
 Then /^(?:|I )should not see "([^"]*)"$/ do |text|
-  if page.respond_to? :should
-    page.should have_no_content(text)
-  else
-    assert page.has_no_content?(text)
-  end
+  assert page.has_no_content?(text)
 end
 
 Then /^(?:|I )should display "([^"]*)"$/ do |identif|

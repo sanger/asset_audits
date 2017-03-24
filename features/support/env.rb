76b32186a97e28260f8e26a791383fe3253b4427
James Glover <james.glover@sanger.ac.uk>
2017-03-24 09:40:45 +0000

    Increase Capybara wait time

--- features/support/env.rb
+++ features/support/env.rb
@@ -17,6 +17,7 @@ Capybara.javascript_driver = :poltergeist
 # prefer to use XPath just remove this line and adjust any selectors in your
 # steps to use the XPath syntax.
 Capybara.default_selector = :css
+Capybara.default_max_wait_time = 10
 
 # By default, any exception happening in your Rails application will bubble up
 # to Cucumber so that your scenario will fail. This is a different from how

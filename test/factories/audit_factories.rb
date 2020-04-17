aea84ca1758973c46923d2bd20ef23cb2d505557
Katy Taylor <kt17@sanger.ac.uk>
2020-04-17 10:59:40 +0100

    FactoryBot.sequence isn't a thing

--- test/factories/audit_factories.rb
+++ test/factories/audit_factories.rb
@@ -1,48 +1,55 @@
 # frozen_string_literal: true
-FactoryBot.sequence :barcode_number do |n|
-  "#{n}"
-end
+# FactoryBot.sequence :barcode_number do |n|
+#   "#{n}"
+# end
 
-FactoryBot.sequence :instrument_name do |n|
-  "Instrument #{n}"
-end
+# FactoryBot.sequence :instrument_name do |n|
+#   "Instrument #{n}"
+# end
 
-FactoryBot.sequence :instrument_process_name do |n|
-  "Instrument Process #{n}"
-end
+# FactoryBot.sequence :instrument_process_name do |n|
+#   "Instrument Process #{n}"
+# end
 
-FactoryBot.sequence :instrument_process_key do |n|
-  "instrument_process_#{n}"
-end
+# FactoryBot.sequence :instrument_process_key do |n|
+#   "instrument_process_#{n}"
+# end
 
-FactoryBot.sequence :bed_barcode do |n|
-  "#{n}"
-end
+# FactoryBot.sequence :bed_barcode do |n|
+#   "#{n}"
+# end
 
-FactoryBot.sequence :bed_number do |n|
-  "#{n}"
-end
+# FactoryBot.sequence :bed_number do |n|
+#   "#{n}"
+# end
 
-FactoryBot.sequence :bed_name do |n|
-  "#{n}"
-end
+# FactoryBot.sequence :bed_name do |n|
+#   "#{n}"
+# end
 
 FactoryBot.define :bed do |a|
-  a.name       { |a| FactoryBot.next :bed_name }
-  a.barcode    { |a| FactoryBot.next :bed_barcode }
-  a.bed_number { |a| FactoryBot.next :bed_number }
+  sequence(:name)       { |n| "#{n}" }
+  sequence(:barcode)    { |n| "#{n}" }
+  sequence(:bed_number) { |n| "#{n}" }
+  # a.name       { |a| FactoryBot.next :bed_name }
+  # a.barcode    { |a| FactoryBot.next :bed_barcode }
+  # a.bed_number { |a| FactoryBot.next :bed_number }
 end
 
 FactoryBot.define :instrument do |a|
-  a.barcode  { |a| FactoryBot.next :barcode_number }
-  a.name     { |a| FactoryBot.next :instrument_name }
+  sequence(:name)    { |n| "Instrument #{n}" }
+  sequence(:barcode) { |n| "#{n}" }
+  # a.barcode  { |a| FactoryBot.next :barcode_number }
+  # a.name     { |a| FactoryBot.next :instrument_name }
 
   a.after_build { |instrument| (1..16).each { |bed_number| FactoryBot(:bed, name: "P#{bed_number}", barcode: bed_number, bed_number: bed_number, instrument_id: instrument.id) } }
 end
 
 FactoryBot.define :instrument_process do |a|
-  a.name     { |a| FactoryBot.next :instrument_process_name }
-  a.key      { |a| FactoryBot.next :instrument_process_key }
+  sequence(:name) { |n| "Instrument Process #{n}" }
+  sequence(:key)  { |n| "instrument_process_#{n}" }
+  # a.name     { |a| FactoryBot.next :instrument_process_name }
+  # a.key      { |a| FactoryBot.next :instrument_process_key }
 end
 
 FactoryBot.define :instrument_processes_instrument do |ipi|

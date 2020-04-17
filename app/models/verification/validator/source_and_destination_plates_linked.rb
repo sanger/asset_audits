20225cbccfc5c9a09e3895aa7fc203ac9d917fb9
Katy Taylor <kt17@sanger.ac.uk>
2020-04-17 16:36:32 +0100

    tweaks

--- app/models/verification/validator/source_and_destination_plates_linked.rb
+++ app/models/verification/validator/source_and_destination_plates_linked.rb
@@ -1,14 +1,12 @@
 # frozen_string_literal: true
+
 class Verification::Validator::SourceAndDestinationPlatesLinked < ActiveModel::Validator
   def validate(record)
     record.parse_source_and_destination_barcodes(record.scanned_values).reject do |source_barcode, destination_barcode|
       destination_barcode.blank? || source_barcode.blank?
     end.tap do |source_and_destinations|
-      # search_resource = record.api.search.find(Settings.search_find_source_assets_by_destination_barcode)
       source_and_destinations.all? do |source_barcode, destination_barcode|
-        # search_results = search_resource.all(record.api.plate, barcode: destination_barcode)
         search_results = Sequencescape::Api::V2::Plate.where(barcode: destination_barcode).first.parents
-        # found_barcodes = search_results.map { |p| p.barcode.machine }
         found_barcodes = search_results.map { |p| p.labware_barcode['machine_barcode'] }
         valid_source_barcode?(source_barcode, found_barcodes, record, destination_barcode)
       end

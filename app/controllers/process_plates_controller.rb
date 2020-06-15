8b1677a5e07f771d0f520b624f20ddd0bd05fcd1
Katy Taylor <kt17@sanger.ac.uk>
2020-06-15 17:19:12 +0100

    Start on the front end - Be able to display responses from lighthouse / wrangler in a table - change to 'render' not 'redirect_to' to allow updating view with results without using flash (flash can't handle a lot of data) - new view for this called 'results' - problem with rendering the 'new' view/form underneath is that the URL still says /process_plates, not /process_plates/new (due to render not redirect), so page won't work if you refresh it - TODO - have broken existing flash messages, fix them - TODO - show red failure flash if some of the service calls failed - TODO - modify the services to return me more information that I can display in my table

--- app/controllers/process_plates_controller.rb
+++ app/controllers/process_plates_controller.rb
@@ -16,7 +16,7 @@ class ProcessPlatesController < ApplicationController
   end
 
   def create
-    respond_to do |format|
+    # respond_to do |format|
       bed_verification_model = InstrumentProcessesInstrument.get_bed_verification_type(params[:instrument_barcode], params[:instrument_process])
       if bed_verification_model.nil?
         flash[:error] = 'Invalid instrument or process'
@@ -33,33 +33,46 @@ class ProcessPlatesController < ApplicationController
           # find out if the 'receive_plates' process was executed
           receive_plates_process = InstrumentProcess.find_by(id: params[:instrument_process]).key.eql?('slf_receive_plates')
 
-          call_external_services(barcodes) if barcodes && receive_plates_process
+          message = ''
+          if barcodes && receive_plates_process
+            call_external_services(barcodes)
+
+            message = @lighthouse_responses.concat(@wrangler_responses)
+          end
+
+          # TODO: display the responses from the services in a partial, not a flash
+          # TODO: sort out original flash code below
+          flash[:notice] = message
 
           # add a flash on the page for the number of unique barcodes scanned in
-          num_unique_barcodes = bed_layout_verification.process_plate&.barcodes.uniq.length
-          if num_unique_barcodes
-            flash[:notice] = "Success - #{num_unique_barcodes} unique plate(s) scanned"
-          else
-            flash[:notice] = "Success"
-          end
+          # num_unique_barcodes = bed_layout_verification.process_plate&.barcodes.uniq.length
+          # if num_unique_barcodes
+          #   flash[:notice] = "Success - #{num_unique_barcodes} unique plate(s) scanned"
+          # else
+          #   flash[:notice] = "Success"
+          # end
         else
           flash[:error] = bed_layout_verification.errors.values.flatten.join("\n")
         end
       end
-      format.html { redirect_to(new_process_plate_path) }
-    end
+      @results = parse_responses
+      render :results
+    # end
   end
 
   # Call any external services - currently lighthouse service for plates from Lighthouse Labs and
   #   wrangler for tube racks. If no samples are found in the lighthouse service, try the wrangler
   def call_external_services(barcodes)
+    puts "DEBUG: call_external_services"
     # call the lighthouse service first as we are assuming that most labware scanned will be
     #   plates from Lighthouse Labs
-    lighthouse_responses = Lighthouse.call_api(barcodes)
+    @lighthouse_responses = Lighthouse.call_api(barcodes)
+    puts "DEBUG: lighthouse_responses: #{@lighthouse_responses}"
 
     # keeping it simple for now, if all the responses are not CREATED, send ALL the barcodes
     #   to the wrangler
-    wrangler_responses = Wrangler.call_api(barcodes) unless all_created?(lighthouse_responses)
+    @wrangler_responses = Wrangler.call_api(barcodes) unless all_created?(@lighthouse_responses)
+    puts "DEBUG: wrangler_responses: #{@wrangler_responses}"
   end
 
   # Returns a list of unique barcodes by removing blanks and duplicates
@@ -72,5 +85,49 @@ class ProcessPlatesController < ApplicationController
     return false if responses.empty?
 
     responses.all? { |response| response[:code] == "201" }
+  end
+
+  def parse_responses
+    output = []
+
+    @lighthouse_responses.each do |r|
+
+      barcode = r[:barcode]
+      success = if r[:code] == '201'
+                  'Yes'
+                else
+                  'No'
+                end
+      purpose = r[:purpose]
+      study = r[:study]
+
+      output << {
+        :barcode => barcode,
+        :success => success,
+        :purpose => purpose,
+        :study => study
+      }
+    end
+
+    @wrangler_responses.each do |r|
+
+      barcode = r[:barcode]
+      success = if r[:code] == '201'
+                  'Yes'
+                else
+                  'No'
+                end
+      purpose = r[:purpose]
+      study = r[:study]
+
+      output << {
+        :barcode => barcode,
+        :success => success,
+        :purpose => purpose,
+        :study => study
+      }
+    end
+
+    output
   end
 end

f8cdb9ea1fa59dbe7fbfd1ca8bf856d5a72169f4
Andrew Sparkes <as28@sanger.ac.uk>
2020-07-13 11:29:12 +0100

    rubocopped

--- app/models/verification/dilution_plate/hamilton.rb
+++ app/models/verification/dilution_plate/hamilton.rb
@@ -2,6 +2,6 @@
 
 # Either 1 or 2 plates can be processed at a time.
 class Verification::DilutionPlate::Hamilton < Verification::DilutionPlate::Base
-  self.source_beds      = %w(P4 P5)
-  self.destination_beds = %w(P34 P35)
+  self.source_beds      = %w[P4 P5]
+  self.destination_beds = %w[P34 P35]
 end

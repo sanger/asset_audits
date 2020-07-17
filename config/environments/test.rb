db8519157b6a74bfb6bd44e455ffa724c8ed7d2d
Andrew Sparkes <as28@sanger.ac.uk>
2020-07-17 14:43:56 +0100

    Removed API call to lighthouse on receiving plates. Lighthouse plates are now received into Sequencescape via a different process.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -46,7 +46,4 @@ Rails.application.configure do
 
   # https://github.com/sanger/wrangler
   config.wrangler_url = 'http://example.com/wrangle'
-
-  # https://github.com/sanger/lighthouse
-  config.lighthouse_host = 'http://127.0.0.1:5000'
 end

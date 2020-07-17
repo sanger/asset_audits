6723a388d6078dcaba0fcde8b6c26d478eaa5600
Andrew Sparkes <andrewsparkes@users.noreply.github.com>
2020-07-17 09:28:10 +0100

    Revert "Gpl424 Bed verification hamilton lysates extraction"

--- app/models/verification/dilution_plate/hamilton.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Either 1 or 2 plates can be processed at a time.
-class Verification::DilutionPlate::Hamilton < Verification::DilutionPlate::Base
-  self.source_beds      = %w[P4 P5]
-  self.destination_beds = %w[P34 P35]
-end

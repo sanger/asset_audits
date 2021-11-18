c4093a9bcb1e88e1a50cb9e8ebd51b031aa9b170
James Glover <james.glover@sanger.ac.uk>
2021-11-18 16:39:00 +0000

    Fix Rubocop Style/OpenStructUse
    See https://msp-greg.github.io/rubocop/RuboCop/Cop/Style/OpenStructUse.html
    
    > This cop flags uses of OpenStruct, as it is now officially discouraged to be used for performance, version compatibility, and potential security issues.
    
    So these changes do the following:
    
    - Replace the mock barcode with a simple Struct. I'd have loved to use a
      double here, but the client api actually returns an anonymous class.
    
    - Remove use of OpenStruct in setting up logging
      - Replace Deployed::DETAILS with hash
      - PsdFormatter expects hash
    
      These two uses are hard to connect together in the code itself, and are
      linked when the formatter is set-up in the deployment project.
      Essentially Deployed::DETAILS gets passed into the initializer of
      PsdFormatter. This change passes a simple hash in instead. It also
      removes :name form the configuration, as this was set to nil,
      so is immediately removed by the compact. The name *is* still correctly
      logged.

--- test/support/test_search_result.rb
+++ test/support/test_search_result.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class TestSearchResult
+  MockBarcode = Struct.new(:ean13, :machine)
+
   attr_accessor :barcode, :uuid
 
   def initialize(machine_barcode, uuid: 'uuid')
     # Not what actually happens, but it'll work for our purposes
     encoded_barcode = machine_barcode.bytes.map { |b| (b % 48).to_s }.join
     @uuid = uuid
-    @barcode = OpenStruct.new(ean13: encoded_barcode, machine: machine_barcode) # rubocop:todo Style/OpenStructUse
+    @barcode = MockBarcode.new(encoded_barcode, machine_barcode)
   end
 end

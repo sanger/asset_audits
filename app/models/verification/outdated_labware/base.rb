9797d554a72141bfb092717f2ce27e5179f61609
Katy Taylor <kt17@sanger.ac.uk>
2022-04-13 16:26:30 +0100

    Remove search_resource as is no longer used - remove these attr_accessors - if they are used, I can't see where

--- app/models/verification/outdated_labware/base.rb
+++ app/models/verification/outdated_labware/base.rb
@@ -6,16 +6,10 @@
 class Verification::OutdatedLabware::Base < Verification::Base
   validates_with Verification::Validator::OutdatedPlatesScanned
 
-  attr_accessor :plate_barcodes_to_destroy, :messages
-
   self.partial_name = 'outdated_labware'
 
   def scanned_values
     [@attributes[:scanned_values]].flatten.map { |s| s.split(/\s/).reject(&:blank?) }.flatten
-  end
-
-  def search_resource
-    api.search.find(Settings.search_find_assets_by_barcode)
   end
 
   def plates_from_barcodes(barcodes)

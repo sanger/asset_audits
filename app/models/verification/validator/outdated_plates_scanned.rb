0b14a36056dd42f4717e723e269ec39d858d9b15
Katy Taylor <kt17@sanger.ac.uk>
2022-04-13 16:18:03 +0100

    Update user-facing text to refer to 'labware' rather than 'plate'

--- app/models/verification/validator/outdated_plates_scanned.rb
+++ app/models/verification/validator/outdated_plates_scanned.rb
@@ -6,18 +6,18 @@ class Verification::Validator::OutdatedPlatesScanned < ActiveModel::Validator
     plates = record.plates_from_barcodes(record.scanned_values)
     plates.each do |barcode, plate|
       if plate.nil?
-        record.errors.add(:error, "The plate #{barcode} hasn't been found")
+        record.errors.add(:error, "The labware #{barcode} hasn't been found")
         next
       end
       if plate.purpose.lifespan.nil?
-        record.errors.add(:error, "The plate #{barcode} can't be destroyed because it's a #{plate.purpose.name}")
+        record.errors.add(:error, "The labware #{barcode} can't be destroyed because it's a #{plate.purpose.name}")
         next
       end
       next unless plate.created_at > plate.purpose.lifespan.days.ago
-      record.errors.add(:error, "The plate #{barcode} is less than #{plate.purpose.lifespan} days old")
+      record.errors.add(:error, "The labware #{barcode} is less than #{plate.purpose.lifespan} days old")
       next
     end
-    record.errors.add(:base, 'No plates found') if plates.empty?
+    record.errors.add(:base, 'No labware found') if plates.empty?
   end
   # rubocop:enable Metrics/MethodLength
 end

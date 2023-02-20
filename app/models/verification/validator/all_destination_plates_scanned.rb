d60f45f045799903a4f871829e1a0b7c20387c4d
Sangeetha <sb67@sanger.ac.uk>
2023-02-20 12:09:14 +0000

    Disabled Rubocop

--- app/models/verification/validator/all_destination_plates_scanned.rb
+++ app/models/verification/validator/all_destination_plates_scanned.rb
@@ -2,11 +2,13 @@
 
 class Verification::Validator::AllDestinationPlatesScanned < ActiveModel::Validator
   def validate(record)
+    # rubocop:disable Style/BlockDelimiters
     unless record.destination_beds.any? { |destination_bed|
              record.scanned_values[destination_bed.downcase.to_sym][:plate].blank?
            }
       return
     end
+    # rubocop:enable Style/BlockDelimiters
 
     record.errors.add(:base, "All destination plates must be scanned")
   end

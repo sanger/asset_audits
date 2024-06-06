58e9df69d1fb7751874e662156f9569f46e125e9
yoldas <ay6@sanger.ac.uk>
2024-06-06 04:59:29 +0100

    Use net/http instead of rest-client for the LabWhere API call

--- app/models/verification/outdated_labware/base.rb
+++ app/models/verification/outdated_labware/base.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "net/http"
+
 #
 # Takes a list of source labware and validates that all labware
 # are older than the lifespan defined by their purpose
@@ -39,38 +41,66 @@ class Verification::OutdatedLabware::Base < Verification::Base
     super
   end
 
+  private
+
   # Sends a POST request to the LabWhere API to scan the labware into the
   # destroyed location. The parameters passed to the ProcessPlatesController
   # for the Destroying labware process are used to construct the payload.
   # If an error occurs, the error message will be added to the errors object,
   # with the LabWhere attribute.
   #
-  # @param params [Hash] the parameters passed to the process_plates action
+  # @param params [Hash] the parameters passed to the create action
   #
   # @return [Boolean] returns true if the request was successful, and false
   # if an error occurred.
-  # :reek:TooManyStatements
   def scan_into_destroyed_location(params)
-    destroyed_location_uri = URI.join(Settings.labwhere_api, "scans").to_s
-    RestClient.post(destroyed_location_uri, destroyed_location_payload(params))
-    true
-  rescue Errno::ECONNREFUSED, RestClient::NotFound
+    response = send_scan_request(params)
+    handle_scan_response(response)
+  rescue Errno::ECONNREFUSED
     errors.add(:LabWhere, "LabWhere service is down")
     false
-  rescue RestClient::UnprocessableEntity => e
-    errors.add(:LabWhere, JSON.parse(e.response.body)["errors"].join(", "))
+  end
+
+  # Sends a POST request to the LabWhere API to scan the abware into the
+  # destroyed location.
+  #
+  # @param params [Hash] The parameters to prepare the scan request.
+  # @return [Net::HTTPResponse] The HTTP response from the LabWhere API.
+  def send_scan_request(params)
+    uri = URI.join(Settings.labwhere_api, "scans")
+    data = destroyed_location_payload(params).to_json
+    headers = { "Content-Type" => "application/json" }
+    Net::HTTP.post(uri, data, headers)
+  end
+
+  # Handles the response from the LabWhere API after scanning the labware into
+  # destroyed location.
+  #
+  # @param response [Net::HTTPResponse] the response from the LabWhere API
+  # @return [Boolean] Returns true if the response is a success, false otherwise.
+  #
+  def handle_scan_response(response)
+    return true if response.is_a?(Net::HTTPSuccess)
+
+    case response
+    when Net::HTTPNotFound
+      errors.add(:LabWhere, "LabWhere service is down")
+    when Net::HTTPUnprocessableEntity
+      errors.add(:LabWhere, JSON.parse(response.body)["errors"].join(", "))
+    else
+      errors.add(:LabWhere, "An error occurred while scanning the labware")
+    end
+
     false
   end
 
   # Constructs the payload for a scan into the LabWhere destroyed location. The
-  # parameters passed to the ProcessPlatesController for the Destroying labware
-  # process are used to construct the payload. The payload is a hash with a
-  # single key, :scan, which maps to another hash. This inner hash contains
-  # the following keys:
+  # returned payload is a hash with a single key, :scan, which maps to another
+  # hash. This inner hash contains the following keys:
   # - :user_code: the swipe card or barcode code of the user performing the scan
   # - :labware_barcodes: a newline-separated string of the barcodes
   # - :location_barcode: the barcode of the destroyed location
-
+  #
   # @param params [Hash] the parameters passed to the create action
   #
   # @return [Hash] the constructed payload

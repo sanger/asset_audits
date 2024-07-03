536792e65bfed7c1cb4bb666bb0745ce028f3765
Stuart McHattie <sm49@sanger.ac.uk>
2024-07-03 14:26:55 +0100

    Apply prettier

--- test/support/mock_sequencescape_v2.rb
+++ test/support/mock_sequencescape_v2.rb
@@ -3,37 +3,28 @@ module MockSequencescapeV2
 
   def mock_get_plate(barcode, attributes)
     plate_json = {
-      "data": [
-        {
-          "id": "1",
-          "type": "plates",
-          "links": {
-            "self": "http://localhost:3000/api/v2/plates/1"
-          },
-          "attributes": attributes
-        }
+      data: [
+        { id: "1", type: "plates", links: { self: "http://localhost:3000/api/v2/plates/1" }, attributes: attributes }
       ]
     }
 
-    stub_request(:get, "http://sequencescape/api/v2/plates?filter%5Bbarcode%5D%5B0%5D=#{barcode}")
-      .with(headers: {
-        'Accept'=>'application/vnd.api+json',
-        'Content-Type'=>'application/vnd.api+json',
-        'X-Sequencescape-Client-Id'=>'cucumber'
-      })
-      .to_return_json(status: 200, body: plate_json)
+    stub_request(:get, "http://sequencescape/api/v2/plates?filter%5Bbarcode%5D%5B0%5D=#{barcode}").with(
+      headers: {
+        "Accept" => "application/vnd.api+json",
+        "Content-Type" => "application/vnd.api+json",
+        "X-Sequencescape-Client-Id" => "cucumber"
+      }
+    ).to_return_json(status: 200, body: plate_json)
   end
 
   def mock_post_asset_audit(body)
-    stub_request(:post, "http://sequencescape/api/v2/asset_audits")
-      .with(
-        body:,
-        headers: {
-          'Accept'=>'application/vnd.api+json',
-          'Content-Type'=>'application/vnd.api+json',
-          'X-Sequencescape-Client-Id'=>'cucumber'
-        }
-      )
-      .to_return_json(status: 200, body: {})
+    stub_request(:post, "http://sequencescape/api/v2/asset_audits").with(
+      body:,
+      headers: {
+        "Accept" => "application/vnd.api+json",
+        "Content-Type" => "application/vnd.api+json",
+        "X-Sequencescape-Client-Id" => "cucumber"
+      }
+    ).to_return_json(status: 200, body: {})
   end
 end

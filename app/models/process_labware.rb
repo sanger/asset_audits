12fb974372128f74640d435598109d04e482fa59
yoldas <ay6@sanger.ac.uk>
2024-07-25 17:36:48 +0100

    Revert "Fix event logging, validation, and tube destruction"
    This reverts commit 7ac3076f105fca167d26d1a2e5f0e5e4487c9b05.

--- app/models/process_labware.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# This class is used for both plates and tubes.
-class ProcessLabware < ProcessPlate
-  def asset_uuids_from_plate_barcodes
-    Sequencescape::Api::V2::Labware.where(barcode: barcodes).map(&:uuid)
-  end
-end

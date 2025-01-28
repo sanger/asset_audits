e7f075587aa2ba1b032ea517de119e1bd237ebe4
yoldas <ay6@sanger.ac.uk>
2025-01-28 03:43:15 +0000

    Add doc for instrument loader

--- lib/record_loader/instrument_loader.rb
+++ lib/record_loader/instrument_loader.rb
@@ -2,6 +2,7 @@
 
 # This file was automatically generated via `rails g record_loader`
 require_relative "application_record_loader"
+
 # RecordLoader handles automatic population and updating of database records
 # across different environments
 # @see https://rubydoc.info/github/sanger/record_loader/
@@ -10,6 +11,12 @@ module RecordLoader
   class InstrumentLoader < ApplicationRecordLoader
     config_folder "instruments"
 
+    # Creates the specified instrument if it is not present.
+    #
+    # @param name [String] The name of the section in the YAML file.
+    # @param options [Hash] The options to create the instrument with.
+    #
+    # @return [Instrument] The created or updated instrument.
     def create_or_update!(name, options)
       Instrument.create_with(options).find_or_create_by!(name:)
     end

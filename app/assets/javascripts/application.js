aabd3d5806d9518c2b1b5e9b76de701b9ae2e877
Stephen Hulme <sh51@sanger.ac.uk>
2026-03-12 13:45:17 +0000

    fix: repair build breakage

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,2 @@
+// This file is required to be here by Sprockets so that the build step doesn't fail.
+// Please see public/javascripts/ for where javascript files should be placed for use in the application.
